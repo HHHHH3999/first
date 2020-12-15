e5c9707b88a5197209141c349063ffcbab5df751
ll-970116 <921829848@qq.com>
2020-12-15 21:39:29 +0800

    张友瑜大傻逼

--- app/src/main/java/com/example/p7/MainActivity.java
+++ app/src/main/java/com/example/p7/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 	
 	// 原创太菜 
+		//罗传波 修改 张友瑜 大傻逼
+		
 //什么玩意
         Log.e("tag","hello word");
 //什么玩意
